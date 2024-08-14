f5536f5f0e3980ac06583d412ed7e05c3dbd7549
fairleyv <fairleyvincent@gmail.com>
2024-08-14 13:13:03 -0700

    added icon to navbar and classNames

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import worldIcon from "../src/assets/worldIcon.png"
 
 export default function Navbar () {
     return (
         <div>
             <nav>
-                <h1>my travel journal.</h1>
+                <img className="navbarWorldIcon" src={worldIcon}/>
+                <h1 className="navbarTitle">my travel journal.</h1>
             </nav>
         </div>
     )
