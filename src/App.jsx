7931852a5a3df036198b27836479fb8d683756df
fairleyv <fairleyvincent@gmail.com>
2024-08-16 11:26:13 -0700

    added cardData to app.jsx and mapped over it to create a card array from the data. Also added id to card object and changed the url of the cardImg in the data.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
-import './App.css'
-import Card from "../components/Card"
-import Navbar from "../components/Navbar"
+import './App.css';
+import Card from "../components/Card";
+import Navbar from "../components/Navbar";
+import cardData from "../src/data";
 
 function App() {
+  let cards = cardData.map(card => {
+    return (
+      <Card 
+          key={card.id}
+          {...card}
+      /> )   
+    }
+  )
 
   return (
     <>
       <Navbar />
-      <Card />
+      {cards}
     </>
   )
 }
